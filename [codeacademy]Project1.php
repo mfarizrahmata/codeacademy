26b1f6c4461f907bf1babd426517e3b7fef68e55
mfarizrahmata <102274842+mfarizrahmata@users.noreply.github.com>
2023-01-11 14:50:30 +0700

    Calculate Our Expected Amount of USD in PHP

--- [codeacademy]Project1.php
+++ [codeacademy]Project1.php
@@ -1,5 +1,6 @@
 <?php
 /*
+Calculate Our Expected Amount of USD
 World Traveler
 
 Hello there, friend! We’re exhausted having just returned to New York City from a whirlwind of world travel. We’re bringing all our international currency to the bank to be exchanged for USD. Our travels have made us wise and weary, and we want to ensure we’re being treated fairly. Can you help?
