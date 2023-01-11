e2f5e4086db30ec4b6c62f9edad058468b77697b
mfarizrahmata <102274842+mfarizrahmata@users.noreply.github.com>
2023-01-11 14:49:57 +0700

    Mad Lib Functions PHP

--- [codeacademy]Project2.php
+++ [codeacademy]Project2.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Mad Lib Functions
+Mad Lib Functions PHP
 
 In this project, we’ll use PHP to write a function to fill in a Mad Libs story! Mad Libs are short stories with blank spaces, 
 which get filled in by the user. The result is usually funny (or strange).
